d9c7d0c067d924b18ca12c3adfa81b38de27b656
David <david.chico.m@gmail.com>
2026-09-07 13:00:48 +0200

    Mejorar tarjetas de Pokemon con artwork

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,20 @@
 export default function PokemonCard({ pokemon }) {
+  const imagenUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+
+  const nombreFormateado =
+    pokemon.nombre.charAt(0).toUpperCase() + pokemon.nombre.slice(1);
+
   return (
     <article>
-      <h3>{pokemon.nombre}</h3>
+      <img
+        src={imagenUrl}
+        alt={`Imagen de ${pokemon.nombre}`}
+        width="180"
+        height="180"
+        loading="lazy"
+      />
+
+      <h3>{nombreFormateado}</h3>
       <p>#{pokemon.id}</p>
     </article>
   );
